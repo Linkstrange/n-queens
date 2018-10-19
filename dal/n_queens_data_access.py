eea2f04b5d63e329d3e0115f21c1560801016f42
ALUNETH\Linkstrange <linkstrange@gmail.com>
2018-10-19 01:34:13 -0500

    Database setup eased up

--- dal/n_queens_data_access.py
+++ dal/n_queens_data_access.py
@@ -1,13 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from configparser import ConfigParser
 
 
 def __get_database_connection_data__():
+    config = ConfigParser()
+    config.read('config.ini')
+    user = config['DEFAULT']['DATABASE_USER']
+    password = config['DEFAULT']['DATABASE_PASS']
+    host = config['DEFAULT']['DATABASE_HOST']
+    name = config['DEFAULT']['DATABASE_NAME']
+
     return 'postgresql://{0}:{1}@{2}/{3}'.format(
-        'postgres',
-        '',
-        'localhost:5432',
-        'nQueens')
+        user,
+        password,
+        host,
+        name)
 
 
 class NQueensDataAccess:
